8f2668f6ea89a42c2313ddb5ead2bd0ae187bd88
Wr8rR <georgiemercury@gmail.com>
2015-02-21 00:26:36 +0100

    changing little mistake in Seq2Sample

--- src/main/scala/Statla.scala
+++ src/main/scala/Statla.scala
@@ -2,7 +2,7 @@ import scala.language.implicitConversions
 
 package object Statla {
   implicit def Seq2Sample[T: Numeric](elems: Seq[T]): SampleLike = elems.length match {
-    case 0 => Empty
+    case 0 => Stats.empty
     case _ => Stats.compute(elems)
   }
 }
