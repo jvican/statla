aa4454ac2cba30a221b6c60ba72aa5f116859f33
jvican <jorgevc@fastmail.es>
2015-02-25 20:28:37 +0100

    now tests passed with implementations of BigDecimal, but it's not right with tests with exact values

--- src/test/scala/SamplesDataSpec.scala
+++ src/test/scala/SamplesDataSpec.scala
@@ -1,29 +1,32 @@
 import java.io.File
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
 
 class SamplesDataSpec extends FlatSpec with Matchers {
   implicit val ds = StRD
+
+  // Be careful, the order of the parameters matter, the first one is the reference to round off
+  def roundOff(first: BigDecimal, second: BigDecimal): BigDecimal = {
+    import scala.math.BigDecimal.RoundingMode._
+    second.setScale(first.scale, HALF_UP)
+  }
 
   behavior of "A Sample"
 
   it should "pass all the datasets" in {
     val testFolder = new File("src/test/datasets/")
-    val filepaths = testFolder.listFiles.toStream.filter(_.getName.endsWith(".dat"))
+    val dataFiles = testFolder.listFiles.toStream.filter(_.getName.endsWith(".dat"))
 
-    filepaths foreach { f =>
+    dataFiles foreach { f =>
       Datasets.read(f) match {
         case Some(res) =>
           val ((rightMean, rightStdev, _), data) = res
 
           println(f.getName)
-          println(Statistics.mean(data))
 
           val sample = Stats.compute(data)
-
-          rightMean should be (sample.mean)
-
-          rightStdev should be (sample.stdev)
+          rightMean should be (roundOff(rightMean, sample.mean))
+          rightStdev should be (roundOff(rightStdev, sample.stdev))
 
         case None => fail("Error reading and interpreting the data and certified values")
       }
